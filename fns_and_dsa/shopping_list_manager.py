879cf022e4556c13cfc7755e245dced93a78fd71
LARRY <tingzlarry@gmail.com>
2025-01-11 15:57:21 +0300

    fixing shopping list manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -9,7 +9,7 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = input("Enter your choice: ").strip()
+        choice = int(input("Enter your choice: "))
 
         if choice == '1':
             item = input("Enter the name of the item to add: ").strip()
