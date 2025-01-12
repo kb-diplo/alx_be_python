5ebc3b3aeba8d1a1d5c1b10f84518b78fb3e7a0d
Mbugualawrencee <143984474+kb-diplo@users.noreply.github.com>
2025-01-12 18:35:15 +0300

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,12 +12,10 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            # Prompt for and add an item
-            item = input("Enter item name: ")
+            item = input("[']Enter item name: ']") 
             shopping_list.append(item)
             print(f"{item} added to the list.")
         elif choice == '2':
-            # Prompt for and remove an item
             item = input("Enter item name to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
@@ -25,7 +23,6 @@ def main():
             else:
                 print(f"{item} not found in the list.")
         elif choice == '3':
-            # Display the shopping list
             if shopping_list:
                 print("Shopping List:")
                 for item in shopping_list:
